9f7a4376a2f25ff9b41ddf5af7652a8977a517e7
saladito <cesarborjamoreno@gmail.com>
2022-03-08 18:43:40 +0100

    p2

--- P2/P2/P2.m
+++ P2/P2/P2.m
@@ -10,6 +10,8 @@ ytest = datos2(:,1);  % Precio en Euros
 Xtest = datos2(:,2:4); % Años, Km, CV
 Ntest = length(ytest);
 
+best_grades = [];
+
 %% Selección del grado del polinomio para la antigüedad del coche
 models=[1 1 1 ; 2 1 1; 3 1 1; 4 1 1; 5 1 1; 6 1 1; 7 1 1; 8 1 1; 9 1 1; 10 1 1];
 [w,best_model,RMSEtr,RMSEcv] = kfold_cross_validation(false,Xdatos,ydatos,10,models);
@@ -22,6 +24,8 @@ plot((1:length(RMSEtr)), RMSEtr, 'r-');
 plot((1:length(RMSEcv)), RMSEcv, 'b-');
 plot(best_model, RMSEcv(best_model), 'g.');
 legend('Error de entrenamiento', 'Error de validación', 'Punto ideal');
+
+best_grades = [best_grades;best_model];
 
 %% Selección del grado del polinomio para los kilómetros del coche
 models=[5 1 1 ; 5 2 1; 5 3 1; 5 4 1; 5 5 1; 5 6 1; 5 7 1; 5 8 1; 5 9 1; 5 10 1];
@@ -36,6 +40,8 @@ plot((1:length(RMSEcv)), RMSEcv, 'b-');
 plot(best_model, RMSEcv(best_model), 'g.');
 legend('Error de entrenamiento', 'Error de validación', 'Punto ideal');
 
+best_grades = [best_grades;best_model];
+
 %% Selección del grado del polinomio para la potencia del coche
 models=[5 6 1 ; 5 6 2; 5 6 3; 5 6 4; 5 6 5; 5 6 6; 5 6 7; 5 6 8; 5 6 9; 5 6 10];
 [w,best_model,RMSEtr,RMSEcv] = kfold_cross_validation(false,Xdatos,ydatos,10,models);
@@ -48,6 +54,13 @@ plot((1:length(RMSEtr)), RMSEtr, 'r-');
 plot((1:length(RMSEcv)), RMSEcv, 'b-');
 plot(best_model, RMSEcv(best_model), 'g.');
 legend('Error de entrenamiento', 'Error de validación', 'Punto ideal');
+
+best_grades = [best_grades;best_model]
+
+w
+
+X_exp_test= expandir(Xtest,best_grades);
+err_test = RMSE(w,X_exp_test,ytest)
 
 %% Regularización
 i = 0.00000001;
@@ -68,3 +81,6 @@ plot(lambda, RMSEtr, 'r-');
 plot(lambda, RMSEcv, 'b-');
 plot(lambda(best_model), RMSEcv(best_model), 'g.');
 legend('Error de entrenamiento', 'Error de validación', 'Punto ideal');
+
+X_exp_test = expandir(Xtest,[10 10 10]);
+err_test = RMSE(w,X_exp_test,ytest)
