954ca828745fd8d3f6dfa5b8bbf72f0d0c307aff
saladito <cesarborjamoreno@gmail.com>
2022-04-08 11:24:00 +0200

    entrenammiento y clasificacion bayesiana

--- P5/clasificacionBayesiana.m
+++ P5/clasificacionBayesiana.m
@@ -1,5 +1,12 @@
-function yhat = clasificacionBayesiana( modelo, X)
+function yhat = clasificacionBayesiana(modelo, X)
 % Con los modelos entrenados, predice la clase para cada muestra X
+    ypred = [];
+    for i = 1:width(modelo)
+        Pi = modelo{i}.N / height(X);
+        ypred(:, i) = gaussLog(modelo{i}.mu,modelo{i}.Sigma,X) * Pi;
+    end
+    [~,yhat] = max(ypred,[],2);
+end
 
 
 
