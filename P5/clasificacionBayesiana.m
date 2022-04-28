107e5c4a747b19b00f775a910be09fede06dc005
saladito <cesarborjamoreno@gmail.com>
2022-04-28 16:23:31 +0200

    p6

--- P5/clasificacionBayesiana.m
+++ P5/clasificacionBayesiana.m
@@ -3,6 +3,7 @@ function yhat = clasificacionBayesiana(modelo, X)
     ypred = [];
     for i = 1:width(modelo)
         Pi = modelo{i}.N / height(X);
+        % se multiplica por la probabilidad a priori de la clase i
         ypred(:, i) = gaussLog(modelo{i}.mu,modelo{i}.Sigma,X) * Pi;
     end
     [~,yhat] = max(ypred,[],2);
