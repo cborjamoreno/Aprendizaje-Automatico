b7e99346769444757d7feb73da6f0dc5b6811138
saladito <cesarborjamoreno@gmail.com>
2022-04-29 10:12:04 +0200

    p6

--- P6/P61.m
+++ P6/P61.m
@@ -27,7 +27,31 @@ Xn = normalize(X,'center','mean');
 
 %calcular Sigma, U, A
 Sigma = 1/(nimages-1)*(X'*X);
-[U,A] =  eig(Sigma);
+[U,A] = eig(Sigma);
+
+%ordenar los vectores propios de U según los calores propios de A
+[A,I] = sort(diag(A),'descend');
+U = U(:, I);
+
+%escoger valor de k
+k_escogido = false;
+k = 1;
+while k_escogido == false && k <= (nrows*ncols)
+    if((sum(A(1:k))/sum(A)) > 0.99)
+        k_escogido = true;
+    else
+        k = k+1;
+    end
+end
+
+%calcular Uk
+Uk = U(:,(1:k));
+
+%reducir dimensión de los datos estandarizados
+Z = X*Uk;
+
+
+
 
 %% Use the classifier from previous labs on the projected space
 
