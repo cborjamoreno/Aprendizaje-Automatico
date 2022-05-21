ac1022de7c6b4bf107b76a96f09ab2115de74c5b
saladito <cesarborjamoreno@gmail.com>
2022-05-21 15:40:41 +0200

    fin p7

--- P7/imageQuantization.m
+++ P7/imageQuantization.m
@@ -2,6 +2,7 @@
 % close all;
 %% imagen
 figure(1)
+% Descomentar la imagen que se quiera probar
 im = imread('smallparrot.jpg');
 % im = imread('atardecer.jpeg');
 % im = imread('elcolor.jpg');
@@ -15,33 +16,51 @@ m = size(D,1);
 n = size(D,2);
 
 %% Kmeans 
-array_k = [2 4 8 16 32];
+% Si no se quiere sacar la evolución de J, se recomienda elegir un valor de
+% K fijo por cada ejecución (e.g. array_k = [2]) para disminuir el tiempo
+% de ejecución
+
+print_evolucion = true; % false si no se quiere mostrar la evolución de J y T
+array_k = [2 4 8 16 32 64]; % disminuir a un solo valor de K si no se quiere mostrar la evolución de J
+T = []; % almacén del tiempo de ejecución para cada K
 %% Escoger agrupamiento
 J_k = [];
 
 for i=1:size(array_k,2)
 
-
+    tic
     % inicialización de los centroides en muestras aleatorias
-%     rng('shuffle');
-%     ind_clusters = fix((m).*rand(K,1))';
-%     mu0 = D(ind_clusters,:);
+    rng('shuffle');
+    ind_clusters = fix((m).*rand(K,1))';
+    mu0 = D(ind_clusters,:);
 
 
     K = array_k(i);
-    mu0 = initCentroids(D,K);
+%     mu0 = initCentroids(D,K);
 
     %bucle kmeans
     [mu, c, J] = kmeans(D, mu0);
     j = min(J);
     J_k = [J_k j];
 
+    t = toc;
+    T = [T t];
+    
+    ahorro_percent = ((m*n-(m+K*n))/(m*n))*100;
+    fprintf("Ahorro de espacio con K = %d: %.2f\n", K,ahorro_percent);
+    fprintf("Error de reconstrucción con K = %d: %.2f\n",K,J_k(i));
+
 end
 
-figure(3);
-plot(array_k, J_k, 'r-', 'LineWidth',1);
-title('Evolución coste')
-ylabel('Coste'); xlabel('K');
+if(print_evolucion)
+    figure(3);
+    plot(array_k, J_k, 'r-', 'LineWidth',1);
+    ylabel('J'); xlabel('K');
+
+    figure(4);
+    plot(array_k, T, 'r-', 'LineWidth',1);
+    ylabel('T'); xlabel('K');
+end
 
 
 
